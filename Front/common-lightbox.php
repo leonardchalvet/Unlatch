fbedb623be4b3fe2f1e0306faf5b345647d3914a
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-09-23 15:18:22 +0200

    Front - Lightbox

---
+++ Front/common-lightbox.php
@@ -0,0 +1,26 @@
+<div class="container-lightbox">
+	<div class="lightbox">
+		<div class="cross">
+			<div class="line"></div>
+			<div class="line"></div>
+		</div>
+		<div class="wrapper">
+			<h3>
+				Plaquette de présentation
+			</h3>
+			<p>
+				Veuillez inscrire votre adresse email afin de pouvoir télécharger notre plaquette.
+			</p>
+			<form>
+				<div class="input">
+					<input type="text" name="email" placeholder="Adresse email">
+					<div class="line"></div>
+				</div>
+				<a class="download" href="path_to_file" download="https://www.peipotato.org/sites/default/files/2017-10/Test%20PDF.pdf" style="display: none;"></a>
+				<button>
+					<span class="btn-text">Télécharger</span>
+				</button>
+			</form>
+		</div>
+	</div>
+</div>
