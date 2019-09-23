0c6f15492bc77740ae82e5b503279d3e61f25ac4
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-09-23 15:11:15 +0200

    Front - 404

---
+++ Front/404.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+
+		<title>404</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/404.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<?php include('common-lightbox.php') ?>
+
+		<main>
+
+			
+			<section id="section-404">
+				<div class="wrapper">
+					<h1>
+						<em>Erreur</em>
+						<em>404</em>
+					</h1>
+					<div class="sep"></div>
+					<p>
+						La page que vous recherchez est introuvable.<br>
+						Lorem ipsum dolor sit amet
+					</p>
+					<div class="container-btn">
+						<a href="">
+							<span class="btn-text">Accueil</span>
+						</a>
+					</div>
+				</div>
+			</section>
+			
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/common-min.js"></script>
+	</body>
+</html>
