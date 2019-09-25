e7741674ef74cd85fdc84ada2ebdd0fbedc16902
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-09-25 18:07:12 +0200

    Front - Html - Clients & Press

---
+++ Front/pres.php
@@ -0,0 +1,227 @@
+<html>
+	<head>
+
+		<title>Clients</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/clients.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section id="section-cover">
+				<div class="wrapper">
+					<div class="container-text">
+						<h1>Ils nous font <span>confiance</span></h1>
+						<div class="container-quotes">
+							<img class="obj-1" src="img/common/icn-quote-white.svg" alt="">
+							<h3>
+								Ces technologie qui mettent l’immobilier en mouvement
+							</h3>
+							<q>
+								Parapher une vente immobilière depuis un ordinateur, une tablette ou un smartphone est désormais possible grâce à la signature électronique…
+							</q>
+							<div class="logo">
+								<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="container-img">
+						<img src="img/common/section-quotes/quote-img-1.jpg" alt="">
+					</div>
+				</div>
+			</section>
+
+			<section id="section-pres">
+				<div class="wrapper">
+					<h2>Les communiqués de <span>presse</span></h2>
+					<div class="container-el">
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+					</div>
+					<div class="container-btn">
+						<a href="" class="elAnim__slide anim__delayMedium_3">
+							<span class="link-text">
+								Voir tous les communiqués
+							</span>
+							<span class="link-arrow">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+									<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+								</svg>
+							</span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-media">
+				<div class="wrapper">
+					<h2>Unlatch dans les <span>médias</span></h2>
+					<div class="container-el">
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+						<div class="el">
+							<img src="img/home/section-logos/logo-haagendazs.png" alt="">
+							<h3>Ces technologie qui mettent l’immobilier en mouvement</h3>
+							<q>
+								Nous avons été le premier promoteur à faire confiance aux équipes de LegaLife. Après un premier test concluant sur un programme nous avons pris la décision de déployer la solution sur l’ensemble de nos programmes, nos équipes étant pleinement satisfaites de l’outil et du gain de temps procuré.
+							</q>
+							<a class="elAnim__slide anim__delayMedium_3">
+								<form method="get" action="file.doc">
+								   <button type="submit">Télécharger le PDF</button>
+								</form>
+								<span class="link-arrow">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+										<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+									</svg>
+								</span>
+							</a>
+						</div>
+					</div>
+					<div class="container-btn">
+						<a href="" class="elAnim__slide anim__delayMedium_3">
+							<span class="link-text">
+								Voir tous les communiqués
+							</span>
+							<span class="link-arrow">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 13">
+									<use xlink:href="img/common/arrow-1.svg#arrow-1"></use>
+								</svg>
+							</span>
+						</a>
+					</div>
+				</div>
+			</section>
+			
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/common-min.js"></script>
+	</body>
+</html>
