b4b4b2d269642097babf58233d9ac335407d5059
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-09-25 16:51:13 +0200

    Prismic init + header + home

---
+++ public/script/js/common.js
@@ -0,0 +1,90 @@
+
+// @codekit-prepend 'jQuery.3.3.1.js'
+
+/*========================================================================
+================================= COMMON =================================
+========================================================================*/
+
+
+
+$(window).on('load', function() {
+	$window = $(window);
+	function animScroll() {
+		var windowHeight = $window.height() / 1.2;
+	    $('.sectionAnim_container').each(function() {
+	        if ($window.scrollTop() >= $(this).offset().top - windowHeight + (windowHeight/4)) {
+	        	if (!$(this).hasClass('reach')) {
+	                $(this).addClass('reach');
+	            }
+	        }
+	    });
+	};
+	$window.scroll(function() {
+	    animScroll();
+	});
+	animScroll();
+})
+
+
+$('#header-desktop .link-solutions').click(function(){
+	$('#header-desktop').removeClass('open-dropdown-features open-dropdown-entreprise');
+	if ($('#header-desktop').hasClass('open-dropdown-solutions')) {
+		$('#header-desktop').removeClass('open-dropdown-solutions');
+	} else {
+		$('#header-desktop').addClass('open-dropdown-solutions');
+	}
+})
+
+$('#header-desktop .link-features').click(function(){
+	$('#header-desktop').removeClass('open-dropdown-solutions open-dropdown-entreprise');
+	if ($('#header-desktop').hasClass('open-dropdown-features')) {
+		$('#header-desktop').removeClass('open-dropdown-features');
+	} else {
+		$('#header-desktop').addClass('open-dropdown-features');
+	}
+})
+
+$('#header-desktop .link-entreprise').click(function(){
+
+	$('#header-desktop').removeClass('open-dropdown-solutions open-dropdown-features');
+	if ($('#header-desktop').hasClass('open-dropdown-entreprise')) {
+		$('#header-desktop').removeClass('open-dropdown-entreprise');
+	} else {
+		$('#header-desktop').addClass('open-dropdown-entreprise');
+	}
+})
+
+$('main').click(function(){
+	$('#header-desktop').removeClass('open-dropdown-product open-dropdown-features open-dropdown-entreprise');
+})
+
+$('em').each(function(){
+	$(this).replaceWith('<span>' + $(this).html() +'</span>');
+})
+
+$('a.openLightbox').click(function(){
+	$('.container-lightbox').addClass('displayBlock');
+	setTimeout(function(){
+		$('.container-lightbox').addClass('show');
+	})
+})
+
+$('.container-lightbox .lightbox .cross').click(function(){
+	$('.container-lightbox').removeClass('show');
+	setTimeout(function(){
+		$('.container-lightbox').removeClass('displayBlock');
+	}, 500)
+})
+
+$(document).click(function(){
+	if (!$(event.target).closest('.container-lightbox .lightbox').length) {
+		if($('.container-lightbox').hasClass('show')) {
+			$('.container-lightbox .lightbox .cross').click();
+		}
+	}
+})
+
+
+/*============================================================================
+================================= END COMMON =================================
+============================================================================*/
---
+++ public/script/js/contact.js
@@ -0,0 +1,24 @@
+$(window).on('load', function() {	
+
+
+	$('#section-contact .container-form form .label input').focusin(function() {
+		$(this).closest('.label').addClass('focus');
+	})
+
+	$('#section-contact .container-form form .label input').focusout(function() {
+		$('#section-contact .container-form form .label').removeClass('focus');
+
+		
+		$('#section-contact .container-form form .label input').each(function(){
+		   if($(this).val()!=""){
+		      	$(this).closest('.label').addClass('fill');
+		    } else {
+		    	$(this).closest('.label').removeClass('fill');
+		    }
+		 });
+
+		
+	})
+
+
+})
