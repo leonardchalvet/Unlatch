b99ca3cde3c0ea36715c842710cc7ac91ce39b58
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-09-26 16:27:22 +0200

    Prismic - MAJ

---
+++ public/script/js/clients.js
@@ -0,0 +1,95 @@
+
+// @codekit-prepend 'common.js'
+
+
+/*=============================================================================
+================================= BEFORE LOAD =================================
+=============================================================================*/
+function sectionQuotesCaroussel(Delay, Section, El, Img, Nav){
+
+	El = Section + ' ' + El;
+	Img = Section + ' ' + Img;
+	Nav = Section + ' ' + Nav;
+
+	var valDelay = 0;
+	var numberEl = $(El).length;
+	var countEl = 1;
+	
+	var drtc;
+
+	function prg(drtc){
+
+		var elImg = Img;
+
+		if (drtc === 'next') {
+			countEl++;
+		} else if (drtc === 'prev') {
+			countEl--;
+		};
+
+		if (countEl <= numberEl && countEl >= 1) {
+
+			$(El + '.active').removeClass('active');
+			$(elImg + '.active').removeClass('active').addClass('hide-after');
+
+			$(El + ':nth-child('+countEl+')').addClass('active');
+			$(elImg + ':nth-child('+countEl+')').removeClass('hide-before').addClass('active');
+
+
+			setTimeout(function() {
+				$(elImg + '.hide-after').removeClass('hide-after').addClass('hide-before');
+			}, 1000);
+
+
+			clearInterval(interval);
+			interval = setInterval(function() {
+		      prg('next');
+		    }, valDelay);
+		} else if (countEl < 1) {
+			countEl = numberEl;
+			prg();
+		} else {
+			countEl = 1;
+			prg();
+		};
+		
+	};
+
+	function init(){	    
+		$(El + ':nth-child(1)').addClass('active');
+		$(Img + ':nth-child(1)').addClass('active');
+		$(Img + '.active').nextAll().addClass('hide-before');
+	};
+
+	$(Nav + ':nth-child(1)').click(function(){
+		clearInterval(interval);
+		prg('next');
+	})
+	$(Nav + ':nth-child(2)').click(function(){
+		clearInterval(interval);
+		prg('prev');
+	})
+
+	init();
+
+	var interval = setInterval(function() {
+    	prg('next');
+    }, valDelay);
+
+    valDelay = Delay;
+    
+
+};
+/*=================================================================================
+================================= END BEFORE LOAD =================================
+=================================================================================*/
+$(window).on('load', function() {	
+
+	sectionQuotesCaroussel(
+		10000,
+		'#section-cover',  
+		".container-el .el",
+		".container-img .el", 
+		'.container-nav .nav'
+	);
+})
