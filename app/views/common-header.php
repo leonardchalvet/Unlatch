69f0d57fd61042ad57ba95c802f078625b09ec0c
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-09-27 15:58:07 +0200

    Prismic - MAJ + Header

--- app/views/common-header.php
+++ app/views/common-header.php
@@ -10,11 +10,15 @@ $header = $WPGLOBAL['header']->data;
 				<img src="<?= $header->hc_logo->url; ?>" alt="">
 			</a>
 			<ul class="container-link">
-				<?php foreach ($header->hc_links as $link) { ?>
+				<?php $i = 1; 
+					foreach ($header->hc_links as $link) { ?>
 					<li>
 						<a <?php if($link->hc_link_ifdropdown == 'no') { 
-								echo 'href="' .$link->hc_link->url. '"'; 
-							} ?> >
+									echo 'href="' .$link->hc_link->url. '"';
+								 } else {
+								 	echo 'class="link-'.$i.'"';
+								 	$i++;
+								 } ?> >
 							<span><?= RichText::asText($link->hc_link_text); ?></span>
 							<?php if($link->hc_link_ifdropdown == 'yes') { 
 								echo '<img src="/img/common/arrow-2.svg" alt="">'; 
@@ -31,187 +35,36 @@ $header = $WPGLOBAL['header']->data;
 			</div>
 		</div>
 	</div>
-	<div class="dropdown dropdown-solutions">
-		<div class="wrapper">
-			<ul>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-1.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Promoteur Immobilier
-							</h4>
-							<p>
-								Unlatch s'adapte à la taille de votre structure et à votre volume de ventes.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-2.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Commercialisateur
-							</h4>
-							<p>
-								Améliorez vos process et apportez un nouveau service à vos clients promoteurs.
-							</p>
-						</div>
-					</a>
-				</li>
-			</ul>
+
+	<?php
+		$i = 0; 
+		foreach ($header->body as $slice) { ?>
+
+		<div class="dropdown dropdown-<?php echo ($i+1); ?>">
+			<div class="wrapper">
+				<ul>
+					<?php foreach ($slice->items as $item) { ?>
+					<li>
+						<a href="">
+							<div class="icn">
+								<img src="<?= $item->hc_dp_icn->url; ?>" alt="">
+							</div>
+							<div class="text">
+								<h4>
+									<?= RichText::asText($item->hc_dp_title); ?>
+								</h4>
+								<p>
+									<?= RichText::asText($item->hc_dp_text); ?>
+								</p>
+							</div>
+						</a>
+					</li>
+					<?php } ?>
+				</ul>
+			</div>
 		</div>
-	</div>
-	<div class="dropdown dropdown-features">
-		<div class="wrapper">
-			<ul>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-10.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Gestion de la relation client (CRM)
-							</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-4.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Espace prescripteur 
-							</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-5.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Digitalisation de la vente immobilière 
-							</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-6.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Suivi des ventes immobilières 
-							</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-7.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Espace client / acheteur 
-							</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-8.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Business intelligence 
-							</h4>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-							</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div class="dropdown dropdown-entreprise">
-		<div class="wrapper">
-			<ul>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-9.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Notre histoire 
-							</h4>
-							<p>
-								Unlatch s'adapte à la taille de votre structure et à votre volume de ventes.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-10.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Nos clients 
-							</h4>
-							<p>
-								Améliorez vos process et apportez un nouveau service à vos clients promoteurs.
-							</p>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<div class="icn">
-							<img src="img/header/link-icn/icn-2.svg" alt="">
-						</div>
-						<div class="text">
-							<h4>
-								Unlatch dans la presse
-							</h4>
-							<p>
-								Améliorez vos process et apportez un nouveau service à vos clients promoteurs.
-							</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-		</div>
-	</div>
+	<?php
+		$i++;
+	} ?>
+	
 </header>
