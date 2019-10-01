4306d850ff60716df28f41e86429c081f49ac9d1
Léonard <leonard@agence-me.com>
2019-10-01 10:34:24 +0900

    Legal Mentions

---
+++ Front/legal-mentions.php
@@ -0,0 +1,95 @@
+<html>
+	<head>
+
+		<title>Mentions Legal</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/legal-mentions.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section id="section-legal-mentions">
+				<div class="wrapper">
+					<h1>
+						<span>Mentions Légales</span> d’Unlatch.
+						
+					</h1>
+					<div class="sep"></div>
+
+					<h3>Edition du site</h3>
+					<p>
+						LegaLife est une société par actions simplifiée au capital de <em>33 411 €</em>, dont le siège social est à Paris (8ème arrondissement) 128, rue la Boétie, immatriculée au registre du commerce et des sociétés de Paris sous le <em>numéro 751 555 723</em> (TVA intracommunautaire n° FR 54 751555723).<br><br>
+						Directeur de la publication : <em>François Marill, président.</em>
+					</p>
+
+					<h4>Informatique & Libertés</h4>
+					<p>
+						Conformément à la loi du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, LegaLife a fait l'objet d'une déclaration auprès de la CNIL conformément à la loi (numéro de déclaration 1700418 v 0).<br> 
+ 						<br> 
+						Vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données qui vous concernent.<br> 
+						<br> 
+						Vous pouvez exercer ce droit en nous contactant via support@legalife.fr ou par courrier postal envoyé à l’adresse suivante :<br> 
+					</p>
+					<ul>
+						<li>
+							LegaLife<br>
+							128, rue la Boétie<br>
+							75008 Paris
+						</li>
+					</ul>   
+					<p>
+						LegaLife a recours à l'utilisation de cookies afin de reconnaître l’Utilisateur lors de sa connexion sur son site. L'Utilisateur peut à tout moment désactiver l'utilisation de ces cookies en sélectionnant les paramètres appropriés dans son navigateur.
+					</p>
+
+					<h4>
+						Propriété Intellectuelle
+					</h4>
+					<p>
+						L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris les représentations iconographiques et photographiques. La reproduction, adaptation et/ou traduction de tout ou partie de ce site sur un support quel qu'il soit, est formellement interdite sauf autorisation expresse du Directeur de la publication.
+					</p>
+
+					<h4>
+						Hébergeur
+					</h4>
+					<p>
+						Le site LegaLife est hébergé en France dans les data centers suivants :
+					</p>
+					<ul>
+						<li>AWS Europe (Paris)</li>
+						<li>OVH Strasbourg SBG1</li>
+					</ul>
+				</div>
+			</section>
+
+			<section id="section-logos">
+				<div class="wrapper">
+					<div class="container-text">
+						<h2>
+							<span>+ de 180 promoteurs</span> immobiliers et <span>commercialisateurs</span> utilisent Unlatch
+						</h2>
+					</div>
+					<div class="container-logos">
+						<img src="img/common-solutions/section-logos/list-logo.png" alt="">
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+	</body>
+
+	<script type="text/javascript" src="script/minify/common-min.js"></script>
+</html>
