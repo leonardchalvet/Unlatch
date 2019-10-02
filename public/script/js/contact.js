d3f10221aa34af5920f7389c82714335e80f95f0
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-10-02 10:45:47 +0200

    Prismic - MAJ

--- public/script/js/contact.js
+++ public/script/js/contact.js
@@ -3,23 +3,31 @@
 $(window).on('load', function() {	
 
 
-	$('#section-contact .container-form form .label input').focusin(function() {
+	$('#section-contact .container-form form .label-text input').focusin(function() {
 		$(this).closest('.label').addClass('focus');
 	})
 
-	$('#section-contact .container-form form .label input').focusout(function() {
-		$('#section-contact .container-form form .label').removeClass('focus');
+	$('#section-contact .container-form form .label-text input').focusout(function() {
+		$('#section-contact .container-form form .label-text').removeClass('focus');
 
-		
-		$('#section-contact .container-form form .label input').each(function(){
+		$('#section-contact .container-form form .label-text input').each(function(){
 		   if($(this).val()!=""){
 		      	$(this).closest('.label').addClass('fill');
 		    } else {
 		    	$(this).closest('.label').removeClass('fill');
 		    }
 		 });
-
 		
+	})
+
+
+	$('#section-contact .container-form form .label-dropdown input').click(function() {
+		$(this).closest('.label').addClass('focus');
+	})
+
+	$('#section-contact .container-form form .label-dropdown .dropdown .el').click(function(){
+		$(this).closest('.label').find('input').val($(this).text());
+		$(this).closest('.label').addClass('fill').removeClass('focus');
 	})
 
 
