304ed3bc3d2efeeffae5557f400a99fae0b4f4d0
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-10-07 14:45:55 +0200

    Add Favicon

--- app/views/blog_author.php
+++ app/views/blog_author.php
@@ -39,6 +39,14 @@ foreach ($articles as $article) {
 
 		<link rel="stylesheet" type="text/css" href="/style/css/blog-author.css">
 
+		<link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+		<link rel="manifest" href="/img/favicon/site.webmanifest">
+		<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#ff4d37">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="theme-color" content="#ffffff">
+
 	</head>
 	
 	<body>
