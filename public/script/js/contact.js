28f14f759f8c989041f74f8e2f11d036be2ddadd
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-10-08 10:47:22 +0200

    Front / Prismic - Contact

--- public/script/js/contact.js
+++ public/script/js/contact.js
@@ -20,9 +20,22 @@ $(window).on('load', function() {
 		
 	})
 
-
 	$('#section-contact .container-form form .label-dropdown input').click(function() {
-		$(this).closest('.label').addClass('focus');
+		if($(this).closest('.label').hasClass('focus')) {
+			$(this).closest('.label').removeClass('focus');
+		}
+		else {
+			$('#section-contact .container-form form .label-dropdown input').closest('.label').removeClass('focus');
+			$(this).closest('.label').addClass('focus');
+		}
+	})
+
+	$(document).click(function(){
+		if (!$(event.target).closest('#section-contact .container-form form .label-dropdown input').length) {
+			if($('#section-contact .container-form form .label-dropdown input').closest('.label').hasClass('focus')) {
+				$('#section-contact .container-form form .label-dropdown input').closest('.label').removeClass('focus');
+			}
+		}
 	})
 
 	$('#section-contact .container-form form .label-dropdown .dropdown .el').click(function(){
