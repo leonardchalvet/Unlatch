dcdb2c34d2a8623476cc0fbbb94af0399ade9f6e
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-10-09 14:35:51 +0200

    Prismic - MAJ + Favicon + Feedback

--- app/views/404.php
+++ app/views/404.php
@@ -19,8 +19,8 @@ $document = $WPGLOBAL['document']->data;
 		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
 		<link rel="manifest" href="/img/favicon/site.webmanifest">
-		<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#ff4d37">
-		<meta name="msapplication-TileColor" content="#ffffff">
+		<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#000000">
+		<meta name="msapplication-TileColor" content="#da532c">
 		<meta name="theme-color" content="#ffffff">
 
 	</head>
